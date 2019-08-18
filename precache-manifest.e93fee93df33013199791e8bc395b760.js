6b42be797017cba0497e613a843f687a167abafa
shbeddy <shbeddy@users.noreply.github.com>
2019-08-18 08:41:38 +0800

    build

---
+++ precache-manifest.e93fee93df33013199791e8bc395b760.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa6d979836fca9630e62d36df221164",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03b44608cfc13a09cd99",
+    "url": "/static/css/2.676dc204.chunk.css"
+  },
+  {
+    "revision": "407ce1919cd9027b5295",
+    "url": "/static/css/main.352bb755.chunk.css"
+  },
+  {
+    "revision": "03b44608cfc13a09cd99",
+    "url": "/static/js/2.2a864075.chunk.js"
+  },
+  {
+    "revision": "407ce1919cd9027b5295",
+    "url": "/static/js/main.7b0d4b52.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
